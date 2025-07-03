72ce0a08f3361fbc6f855abfff379ced191dd439
Nicholas Renotte <nicholas.renotte@gmail.com>
2025-07-04 08:00:02 +1000

    tiny model

--- src/data.py
+++ src/data.py
@@ -1,44 +1,50 @@
-import os 
-import torch 
+import os
+import torch
 from PIL import Image
-import pandas as pd 
+import pandas as pd
 import numpy as np
 from torch.utils.data import Dataset, DataLoader
 import albumentations as A
-from matplotlib import pyplot as plt 
+from matplotlib import pyplot as plt
 
-class ClfDataset(Dataset): 
-    def __init__(self, path): 
+
+class ClfDataset(Dataset):
+    def __init__(self, path):
         super().__init__()
-        self.path = path 
-        self.images = [x for x in os.listdir(self.path) if x.endswith('.png')]
-        self.labels = pd.read_csv(f'{path}/labels.csv').set_index('Image')
-        self.transform = A.Compose([
+        self.path = path
+        self.images = [x for x in os.listdir(self.path) if x.endswith(".png")]
+        self.labels = pd.read_csv(f"{path}/labels.csv").set_index("Image")
+        self.transform = A.Compose(
+            [
                 A.Resize(256, 256),
-                A.RandomResizedCrop((224, 224), scale=(0.95, 1.0), p=0.2),
-                A.Affine(scale={'x': (0.9, 1.1), 'y': (0.9, 1.1)}, p=0.2),
+                A.RandomResizedCrop((224, 224), scale=(0.95, 1.0)),
+                A.Affine(scale={"x": (0.9, 1.1), "y": (0.9, 1.1)}, p=0.2),
                 A.ColorJitter(brightness=0.2, contrast=0.2, saturation=0.2, p=0.2),
                 A.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
                 A.ToTensorV2(),
-            ])
+            ]
+        )
 
-    def __len__(self): 
+    def __len__(self):
         return len(self.images)
-    def __getitem__(self,idx):  
-        img = Image.open(os.path.join(self.path, self.images[idx])).convert('RGB')
-        img_tensor = self.transform(image=np.array(img))
-        print(self.images[idx])
-        # img_tensor = torch.tensor(np.array(img)).permute(2,0,1) / 255.0
-        label = self.labels.loc[self.images[idx]]['Label']
-        return img_tensor['image'], torch.tensor(label)
 
-if __name__ == '__main__': 
-    data = ClfDataset('consolidated_data') 
-    dataloder = DataLoader(data, batch_size=32, shuffle=True, prefetch_factor=2, num_workers=4)
-    X, y = next(iter(dataloder)) 
+    def __getitem__(self, idx):
+        img = Image.open(os.path.join(self.path, self.images[idx])).convert("RGB")
+        img_tensor = self.transform(image=np.array(img))
+        # img_tensor = torch.tensor(np.array(img)).permute(2,0,1) / 255.0
+        label = self.labels.loc[self.images[idx]]["Label"]
+        return img_tensor["image"], torch.tensor(label)
+
+
+if __name__ == "__main__":
+    data = ClfDataset("consolidated_data")
+    dataloder = DataLoader(
+        data, batch_size=32, shuffle=True, prefetch_factor=2, num_workers=4
+    )
+    X, y = next(iter(dataloder))
     print(X, y)
-    for idx, img in enumerate(X): 
+    for idx, img in enumerate(X):
         img_np = img.permute(1, 2, 0).numpy()
         img_min, img_max = img_np.min(), img_np.max()
         img_np_scaled = (img_np - img_min) / (img_max - img_min)
-        plt.imsave(f'transformed_images/{idx}_scaled.png', img_np_scaled)
+        plt.imsave(f"transformed_images/{idx}_scaled.png", img_np_scaled)
--- src/model.py
+++ src/model.py
@@ -1,98 +1,109 @@
 import torch
 from torch import nn
-from colorama import Fore 
+from colorama import Fore
 from einops.layers.torch import Rearrange
 import torchvision
 
-class MLPBlock(nn.Sequential): 
-    def __init__(self, embed_dim, mlp_dim): 
+
+class MLPBlock(nn.Sequential):
+    def __init__(self, embed_dim, mlp_dim):
         super(MLPBlock, self).__init__(
             nn.Linear(embed_dim, mlp_dim),
             nn.GELU(),
-            nn.Dropout(0.0), 
-            nn.Linear(mlp_dim, embed_dim), 
-            nn.Dropout(0.0)
+            nn.Dropout(0.0),
+            nn.Linear(mlp_dim, embed_dim),
+            nn.Dropout(0.0),
         )
 
+
 # Build encoder
-class EncoderBlock(nn.Module): 
-    def __init__(self, embed_dim, num_heads, mlp_dim, batch_first=True): 
+class EncoderBlock(nn.Module):
+    def __init__(self, embed_dim, num_heads, mlp_dim, batch_first=True):
         super().__init__()
         self.ln_1 = nn.LayerNorm(embed_dim)
-        self.self_attention = nn.MultiheadAttention(embed_dim, num_heads, batch_first=batch_first, dropout=0.0)
+        self.self_attention = nn.MultiheadAttention(
+            embed_dim, num_heads, batch_first=batch_first, dropout=0.0
+        )
         self.dropout = nn.Dropout(0.0)
         self.ln_2 = nn.LayerNorm(embed_dim)
         self.mlp = MLPBlock(embed_dim, mlp_dim)
-    
-    def forward(self, x): 
+
+    def forward(self, x):
 
         z1 = self.ln_1(x)
         # Add in pre ln skip
-        z1 = self.self_attention(z1,z1,z1,need_weights=False)[0] + x 
+        z1 = self.self_attention(z1, z1, z1, need_weights=False)[0] + x
         z1 = self.dropout(z1)
-        
+
         z2 = self.ln_2(z1)
         # Add in pre ln skip
         z2 = self.mlp(z2) + z1
         return z2
 
-class Encoder(nn.Module): 
-    def __init__(self, embed_dim, num_heads, mlp_dim, num_layers): 
+
+class Encoder(nn.Module):
+    def __init__(self, embed_dim, num_heads, mlp_dim, num_layers):
         super().__init__()
-        self.layers = nn.Sequential() 
-        for x in range(num_layers): 
-            self.layers.add_module(f"encoder_layer_{x}", EncoderBlock(embed_dim, num_heads, mlp_dim))
-    def forward(self, x): 
-        return self.layers(x) 
+        self.layers = nn.Sequential()
+        for x in range(num_layers):
+            self.layers.add_module(
+                f"encoder_layer_{x}", EncoderBlock(embed_dim, num_heads, mlp_dim)
+            )
 
-def pos_encoding(seq_length, dim_size): 
-    p = torch.zeros((seq_length, dim_size)) 
-    for k in range(seq_length):  
-        for i in range(int(dim_size/2)): 
-            p[k,2*i] = torch.sin(torch.tensor(k/(10000 ** (2*i/dim_size))))
-            p[k,2*i+1] = torch.cos(torch.tensor(k/(10000 ** (2*i/dim_size))))
-    return p 
+    def forward(self, x):
+        return self.layers(x)
 
-# Build the Model 
-class ViT(nn.Module): 
-    def __init__(self): 
-        super().__init__() 
+
+def pos_encoding(seq_length, dim_size):
+    p = torch.zeros((seq_length, dim_size))
+    for k in range(seq_length):
+        for i in range(int(dim_size / 2)):
+            p[k, 2 * i] = torch.sin(torch.tensor(k / (10000 ** (2 * i / dim_size))))
+            p[k, 2 * i + 1] = torch.cos(torch.tensor(k / (10000 ** (2 * i / dim_size))))
+    return p
+
+
+# Build the Model
+class ViT(nn.Module):
+    def __init__(self):
+        super().__init__()
         self.patch = Rearrange("b c (h p1) (w p2) -> b (h w) (p1 p2 c)", p1=16, p2=16)
-        self.class_token = nn.Parameter(torch.randn(1,1,768))
-        self.embedding = nn.Linear(768,768)
-        self.register_buffer('positional_embedding', pos_encoding(197,768)) 
+        self.class_token = nn.Parameter(torch.randn(1, 1, 384))
+        self.embedding = nn.Linear(768, 384)
+        self.register_buffer("positional_embedding", pos_encoding(197, 384))
         self.emb_dropout = nn.Dropout(0.0)
-        self.norm1 = nn.LayerNorm(768) 
-        self.norm2 = nn.LayerNorm(768) 
+        self.norm1 = nn.LayerNorm(768)
+        self.norm2 = nn.LayerNorm(384)
 
-        self.encoder = Encoder(768, 12, 3072, 12) 
-        self.mlp1 = nn.Linear(768, 6)
-        
-    def forward(self,x): 
+        self.encoder = Encoder(384, 12, 1024, 3)
+        self.mlp1 = nn.Linear(384, 6)
+
+    def forward(self, x):
         batch_size = x.shape[0]
-        x = self.patch(x) 
-        x = self.norm1(x) 
+        x = self.patch(x)
+        # x = self.norm1(x)
         x = self.embedding(x)
-        x = self.norm2(x) 
-        # print(Fore.LIGHTMAGENTA_EX + str(x.shape) + Fore.RESET) 
+        # x = self.norm2(x)
+        # print(Fore.LIGHTMAGENTA_EX + str(x.shape) + Fore.RESET)
         class_tokens = self.class_token.expand(batch_size, -1, -1)
         patch_embedding = torch.cat([class_tokens, x], dim=1)
-        final_embedding = patch_embedding + self.positional_embedding 
-        final_embedding_dropout = self.emb_dropout(final_embedding) 
-        # print(Fore.LIGHTCYAN_EX + str(x.shape) + Fore.RESET) 
-        encoded_embedding = self.encoder(final_embedding_dropout) 
-        # print(Fore.LIGHTGREEN_EX + str(encoded_embedding.shape) + Fore.RESET) 
-        attended_class_token = encoded_embedding[:,0,:]
-        # print(Fore.LIGHTRED_EX + str(attended_class_token.shape) + Fore.RESET) 
+        final_embedding = patch_embedding + self.positional_embedding
+        final_embedding_dropout = self.emb_dropout(final_embedding)
+        # print(Fore.LIGHTCYAN_EX + str(x.shape) + Fore.RESET)
+        encoded_embedding = self.encoder(final_embedding_dropout)
+        # print(Fore.LIGHTGREEN_EX + str(encoded_embedding.shape) + Fore.RESET)
+        attended_class_token = encoded_embedding[:, 0, :]
+        # print(Fore.LIGHTRED_EX + str(attended_class_token.shape) + Fore.RESET)
         z1 = self.mlp1(attended_class_token)
 
         return z1
 
-if __name__ == '__main__': 
+
+if __name__ == "__main__":
     model = ViT()
 
-    pretrained = torchvision.models.vit_b_16() 
+    pretrained = torchvision.models.vit_b_16()
     print(model.encoder.layers.load_state_dict(pretrained.encoder.layers.state_dict()))
-    print(model) 
+    print(model)
     # model.eval()
     # print(model(torch.randn(1,3,224,224)))
