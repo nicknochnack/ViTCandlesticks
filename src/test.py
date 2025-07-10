fcb190e592061d954ef62ab6e1fe0d4df3c6026d
Nicholas Renotte <nicholas.renotte@gmail.com>
2025-07-10 10:29:41 +1000

    i really should put this data somewhere else

--- src/test.py
+++ src/test.py
@@ -13,19 +13,21 @@ candles = {
     4:"evening_star"
 }
 
-model = ViT().to("cuda")
-model.load_state_dict(torch.load("checkpoints/50_model.pt", weights_only=True))
+model = ViT()
+model.load_state_dict(torch.load("checkpoints/50_model.pt", weights_only=True, map_location=torch.device('cpu')))
 model.eval()
 
 data = ClfDataset("data/test_data")
 dataloader = DataLoader(
-    data, batch_size=4, shuffle=True, prefetch_factor=2, num_workers=2
+    data, batch_size=4, shuffle=True
+    # , prefetch_factor=2, num_workers=2
 )
 sample = next(iter(dataloader))
-X = sample[0]  # torch.unsqueeze(sample[0], dim=0)
+# import sys; sys.exit()
+X = sample[0] 
 print(X.shape)
 softy = torch.nn.Softmax(dim=1)
-preds = model(X.to("cuda"))
+preds = model(X)
 
 print(softy(preds)) 
 argmaxes = torch.argmax(softy(preds), dim=-1)
@@ -33,7 +35,7 @@ print(argmaxes)
 print(sample[1])
 
 loss_fn = nn.CrossEntropyLoss()
-print(loss_fn(preds, sample[1].to("cuda")))
+print(loss_fn(preds, sample[1]))
 
 fig, ax = plt.subplots(2,2) 
 axs = ax.flatten()
