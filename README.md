5a20c6cec398010e69df59e2149651b99b2ae172
Nicholas Renotte <nicholas.renotte@gmail.com>
2025-07-21 10:41:33 +1000

    did train comments and readme

--- README.md
+++ README.md
@@ -1,14 +1,26 @@
+# Using Vision Transformers to Predict Candlestick Patterns
+Need a reason to go and learn vision transformers? Well...here ya go, one kicka--politically correct project to get you started. 
+
+## See it live and in action 📺 - Click the image!
+<!-- <a href="https://youtu.be/D3pXSkGceY0"><img src="https://i.imgur.com/nEfrhIQ.png"/></a> -->
+TBD - vid coming in hot.
+
+# Setup 🪛
 1. Install UV - `pip install uv`
 2. Clone the repo - `git clone https://github.com/nicknochnack/ViTCandlesticks .`
 3. Install all the dependencies `uv sync`
 
-To Do
-- Load pretrained weights: 
-    https://docs.pytorch.org/vision/main/models/generated/torchvision.models.vit_b_16.html or 
-    
-<!-- Note torchvision parameters. -->
-torchrun --nproc_per_node=8 train.py\
-    --model vit_b_16 --epochs 300 --batch-size 512 --opt adamw --lr 0.003 --wd 0.3\
-    --lr-scheduler cosineannealinglr --lr-warmup-method linear --lr-warmup-epochs 30\
-    --lr-warmup-decay 0.033 --amp --label-smoothing 0.11 --mixup-alpha 0.2 --auto-augment ra\
-    --clip-grad-norm 1 --ra-sampler --cutmix-alpha 1.0 --model-ema
+# Training 🦾
+1. Create a checkpoints folder `mkdir checkpoints`
+2. Run the training pipeline `uv run src/train.py`
+
+# Running  🚀 
+1. In one terminal, startup the candlesticks pipeline from yahoo finance `uv run src/utils/candlesticks.py`
+2. In a separate terminal, run `uv run candlestick_prediction.py`
+<p><strong>P.s.</strong> you might need to tweak the dimensions of the screen capture based on your monitor size in `candlestick_prediction.py`. You can do that in this line via albumentations: `A.Crop(x_min=0, y_min=170, x_max=3840, y_max=2160)`</p>
+
+
+# Who, When, Why?
+👨🏾‍💻 Author: Nick Renotte <br />
+📅 Version: 1.x<br />
+📜 License: This project is licensed under the MIT License </br>
--- src/train.py
+++ src/train.py
@@ -1,24 +1,33 @@
+# Bring in model for training 
 from model import ViT
+# Bring in dataset class
 from data import ClfDataset
+# Bring in torch for setting no grad and manual seed  
 import torch
+# Bring in nn for losses, optim for Adam and save for saving
 from torch import nn, optim, save
+# Bring in Dataloader for batching, random_split for train val, and default collate for mixup/cutmix pipeline
 from torch.utils.data import DataLoader, random_split, default_collate
+# This is my favourite library
 from colorama import Fore
+# Mixup/CutMix 
 from torchvision.transforms import v2
+# Outputting model info 
 from torchinfo import summary
 
-
+# Run the pipeline brev
 if __name__ == "__main__":
+    # Set seed for reproducability - I think that's a real word. 
     torch.manual_seed(42)
-    # Get train and test data 
+    # Setup train and test datasets
     train_val_data = ClfDataset("data/train_data", train=True)
     test_data = ClfDataset("data/test_data", train=False)
-    # Create train and val splits
+    # Split train_val into train and val subsets 
     train_size = int(0.7 * len(train_val_data))
     val_size = len(train_val_data) - train_size
     train_data, val_data = random_split(train_val_data, [train_size, val_size])
 
-    # Implement cut_mix, mix_up for train and val only 
+    # Implement cut_mix, mix_up for train only 
     cutmix = v2.CutMix(num_classes=5)
     mixup = v2.MixUp(num_classes=5)
     cutmix_or_mixup = v2.RandomChoice([cutmix, mixup, v2.Identity()], p=[0.25,0.25,0.5])
@@ -28,37 +37,57 @@ if __name__ == "__main__":
     train_dataset = DataLoader(
         train_data, batch_size=16, shuffle=True, collate_fn=collate_fn,
     )
+    # Create val pipeline - used for hyper tuning and model val
     val_dataset = DataLoader(
         val_data, batch_size=16, shuffle=False, 
     )
+    # Create test pipeline - used for assessing how well this big dawg is going to perform in the real world
     test_dataset = DataLoader(
         test_data, batch_size=16, shuffle=False,
     ) 
-
+    # Instantiate the model 
     model = ViT()
+    # Output model info 
     print(summary(model, (1, 3, 104, 72)))
-    loss_fn = nn.CrossEntropyLoss()
 
+    # How long we'll train for, we'll capture a bunch of checkpoints so we can choose what to use 
     epochs = 100
+    # Good ol cross entropy loss - note this applies the softmax internally 
+    loss_fn = nn.CrossEntropyLoss()
+    # Setup optimizer with a learning rate of 0.0001
     opt = optim.Adam(model.parameters(), lr=1e-4)
+    # Add in a warm restart scheduler to slow learning rate the longer training goes on
     scheduler = optim.lr_scheduler.CosineAnnealingWarmRestarts(opt, len(train_dataset)*30, T_mult=2)
+
+    # Used to show progress over entire dataset
     train_batches = len(train_dataset)
     print(Fore.LIGHTYELLOW_EX + "Starting training" + Fore.RESET)
+    # Run training loop 
     for epoch in range(epochs):
+        # Enable all layers 
         model.train()
+        # Used to accumulate loss over the whole epoch
         epoch_loss = 0.0
         for batch_idx, batch in enumerate(train_dataset):
+            # Grab batch
             X, y = batch
+            # Make preds
             yhat = model(X)
+            # Calc loss
             loss = loss_fn(yhat, y)
+            # Accumulate
             epoch_loss += loss.item()
 
+            # Zero grads
             opt.zero_grad()
+            # Calculate gradients
             loss.backward()
+            # Apply them via backprop 
             opt.step()
+            # Progress on the scheduler
             scheduler.step()
 
-            # Progress bar
+            # Fancy progress bar
             progress = (batch_idx + 1) / train_batches
             bar_length = 30
             filled_length = int(bar_length * progress)
@@ -69,7 +98,7 @@ if __name__ == "__main__":
             )
 
         print(f" - Train Loss: {epoch_loss/train_batches:.4f}", end="")
-        # Evaluate val and test set
+        # Do the same stuff but for validation partition and test partition
         model.eval()
         with torch.no_grad():
             epoch_loss = 0.0
@@ -88,7 +117,9 @@ if __name__ == "__main__":
                 epoch_loss += loss.item()
 
             print(f" - Test Loss: {epoch_loss/len(test_dataset):.4f}")
-
+    
+    # Save the model every 5 epochs
         if epoch % 5 == 0:
             save(model.state_dict(), f"checkpoints/{epoch}_model.pt")
+    # And one final save for good measure 
     save(model.state_dict(), f"checkpoints/{epoch}_model.pt")
